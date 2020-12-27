f2c8257ac35a600a6ad9799bf42622ecde354e7d
Vixine <3858343+Vixine@users.noreply.github.com>
2020-12-27 11:53:49 -0600

    fixed line-height

--- RestyleKosherPrint.js
+++ RestyleKosherPrint.js
@@ -5,7 +5,7 @@ javascript: (function() {
     }
     else{
         document.getElementsByTagName('header')[0].setAttribute('style','background-color:#fff');
-        document.styleSheets[2].insertRule('.print{font-size:14pt!important}','.recipe-print__directions ol>li{line-height:18px!important}','.recipe-print__callout-meta-description{line-height:18px!important}');
+        document.styleSheets[2].insertRule('.print{font-size:14pt!important}','.recipe-print__directions ol>li{line-height:1.2!important}','.recipe-print__callout-meta-description{line-height:1.2!important}');
         let use = document.getElementsByTagName("use");
         for (let i in use) {
             use[i].setAttribute("fill", "#000");
