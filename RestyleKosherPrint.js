129531b8f6c0de513864cea990b9a415eaa00a2a
Vixine <3858343+Vixine@users.noreply.github.com>
2021-01-14 22:05:21 -0600

    switched to iframe to only use bookmarklet once

--- RestyleKosherPrint.js
+++ RestyleKosherPrint.js
@@ -1,20 +1,25 @@
 javascript: (function() {
     let recipeprint = "https://www.kosher.com/recipe/print/" + window.location.pathname.match(/\d+$/g);
-    if(window.location.href != recipeprint){
-        window.open(recipeprint, '_blank');
-    }
-    else{
-        document.getElementsByTagName('header')[0].setAttribute('style','background-color:#fff');
-        let css = document.styleSheets[2];
-        css.insertRule('.print{font-size:14pt!important}'),
-        css.insertRule('.recipe-print__directions ol>li{line-height:1.2!important}'),
-        css.insertRule('.recipe-print__callout-meta-description{line-height:1.2!important}'),
-        css.insertRule('.recipe-print__callout-meta-container::before{font-size:0!important;}'),
-        css.insertRule('.recipe-print__callout{font-size:9pt!important}');
-        let use = document.getElementsByTagName("use");
-        for (let i=0;i<use.length;i++) {
-            use[i].setAttribute("fill","#7a087a");
-        };
-        window.print();
-    }
+
+    let iframe = document.createElement("iframe");
+    iframe.src = recipeprint;
+    iframe.setAttribute("id", "printiframe");
+    document.body.appendChild(iframe);
+    setTimeout(
+        function(){
+            let printframe = document.getElementById("printiframe").contentDocument;
+    
+            let css = printframe.styleSheets[2];
+            css.insertRule('.print{font-size:14pt!important}');
+            css.insertRule('.recipe-print__directions ol>li{line-height:1.2!important}');
+            css.insertRule('.recipe-print__callout-meta-description{line-height:1.2!important}');
+            css.insertRule('.recipe-print__callout-meta-container::before{font-size:0!important;}');
+            css.insertRule('.recipe-print__callout{font-size:9pt!important}');
+            let use = printframe.getElementsByTagName("use");
+            for (let i=0;i<use.length;i++) {
+                use[i].setAttribute("fill","#7a087a");
+            };
+            printframe.getElementsByTagName('header')[0].setAttribute('style','background-color:#fff');
+            document.getElementById("printiframe").contentWindow.print();
+        },500);
 })()
